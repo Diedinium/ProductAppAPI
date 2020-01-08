b24bb37da31f3d81331560ace49fba1b2a7f8bec
Jake Hall <jakethomashall@gmail.com>
2020-01-08 23:02:23 +0000

    Add project files.

---
+++ ProductAppAPI/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using ProductAppAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductAppAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+        // List of products for testing purposes (would query database in real scenario.
+        private List<Product> products = new List<Product>() 
+        {
+            new Product() { ID = 1, Name = "Product 1", Category = "Car", Price = 23.23m},
+            new Product() { ID = 2, Name = "Product 2", Category = "Bike", Price = 23.23m},
+            new Product() { ID = 3, Name = "Product 3", Category = "Shed", Price = 23.23m}
+        };
+
+        // Returns all products as a list
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        // Returns single product based on id.
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.ID == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ ProductAppAPI/Scripts/Custom/productPage.js
@@ -0,0 +1,31 @@
+﻿var uri = "api/product"
+
+$(function () {
+    // Populates list on page load
+    $.getJSON(uri).done(function (data) {
+        $.each(data, function (key, item) {
+            $('<li>', { text: formatItem(item) }).appendTo($("#products"));
+        });
+    });
+    $("button[type='reset']").click(function () {
+        $("#product").html("");
+    });
+});
+
+// Formats item for displaying.
+function formatItem(item) {
+    return item.Name + ': $' + item.Price + " - Category : " + item.Category;
+}
+
+// Returns single result
+function find() {
+    var id = $("#prodID").val();
+    $.getJSON(uri + "/" + id)
+        .done(function (data) {
+            $("#product").text(formatItem(data));
+        })
+        .fail(function (jqXHR, textStatus, err) {
+            $("#product").text("Error: " + err);
+        });
+
+}
